49e0c71cc5006ac771eeea9898242181277f1866
kawakubox <shigefumi.kawakubo@gmail.com>
2016-03-19 14:02:51 +0900

    Implements parse.

--- lib/ghtrend.rb
+++ lib/ghtrend.rb
@@ -6,7 +6,34 @@ module Ghtrend
   def self.trend
     doc = Oga.parse_html(Net::HTTP.get URI.parse('https://github.com/trending'))
     doc.css('li.repo-list-item').map do |repo|
-      {}
+      {
+        name: repo_name(repo),
+        url: URI.join('https://github.com/', repo_path(repo)).to_s,
+        description: repo_description(repo),
+        meta: repo_meta(repo),
+      }
     end
+  end
+
+  private
+
+  def self.repo_name(node)
+    node.css('.repo-list-name > a').map { |e| trim(e.text) }.join
+  end
+
+  def self.repo_path(node)
+    node.at_css('.repo-list-name > a').get('href')
+  end
+
+  def self.repo_description(node)
+    node.css('.repo-list-description').map { |e| trim(e.text) }.join(' ')
+  end
+
+  def self.repo_meta(node)
+    node.css('.repo-list-meta').text.split('•').map { |s| trim(s) }.slice(0, 2).join(', ')
+  end
+
+  def self.trim(s)
+    s.gsub(/\n[ ]*/, '').chomp
   end
 end
--- spec/ghtrend_spec.rb
+++ spec/ghtrend_spec.rb
@@ -11,5 +11,14 @@ describe Ghtrend do
     subject(:result) { Ghtrend.trend }
     it { is_expected.to be_kind_of Array }
     it 'has 25 elements' do expect(result.size).to eq 25 end
+
+    describe 'return any element' do
+      subject(:elem) { result.sample }
+      it { is_expected.to be_kind_of Hash }
+      it { is_expected.to be_has_key :name }
+      it { is_expected.to be_has_key :url }
+      it { is_expected.to be_has_key :description }
+      it { is_expected.to be_has_key :meta }
+    end
   end
 end
